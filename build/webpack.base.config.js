1eb6bc99aebb17144d8874375e135fab759612ac
zhengwj <zhengwj@guahao.com>
2018-10-25 20:45:09 +0800

    feat:分离为客户端config.js

---
+++ build/webpack.base.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+
+const config = {
+  target: 'web',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.[hash:8].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
+
+module.exports = config
