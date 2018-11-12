09eb5616861146fd7282f2217bd1e5c8ab938c36
zhengwj <zhengwj@guahao.com>
2018-11-12 20:59:16 +0800

    feat：添加server.config配置

---
+++ build/webpack.server.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base.config')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+const VueServerPlugin = require('vue-server-renderer/server-plugin')
+
+let config
+config = merge(baseConfig, {
+  target: 'node',
+  entry: path.resolve(__dirname, '../client/server-entry.js'),
+  module: {
+    rules: [{
+      test: /\.styl/,
+      use: ExtractPlugin.extract({
+        fallback: 'vue-style-loader',
+        use: [
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          'stylus-loader'
+        ]
+      })
+    }]
+  },
+  devtool: 'source-map',
+  output: {
+    libraryTarget: 'commonjs2',
+    filename: 'server-entry.js',
+    path: path.join(__dirname, '../server-build')
+  },
+  externals: Object.keys(require('../package.json').dependencies), // node环境使用node_modules目录文件
+  plugins: [
+    new ExtractPlugin('styles.[contentHash:8].css'),
+    new webpack.DefinePlugin({
+      'process.env.NODE.ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.NODE.ENV': '"server"'
+    }),
+    new VueServerPlugin()
+  ]
+})
+
+module.exports = config
