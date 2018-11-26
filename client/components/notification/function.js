570d645139690bfb80044e22524b44200198d699
zhengwj <zhengwj@guahao.com>
2018-11-26 21:38:41 +0800

    注释

--- client/components/notification/function.js
+++ client/components/notification/function.js
@@ -1,16 +1,16 @@
-import Component from './func-notification'
-import Vue from 'vue'
+// import Component from './func-notification'
+// import Vue from 'vue'
 
-const NotificationConstructor = Vue.extend(Component)
+// const NotificationConstructor = Vue.extend(Component)
 
-const instances = []
-let seed = 1
-const notify = (options) => {
-  if (Vue.prototype.$isServer) return
+// const instances = []
+// let seed = 1
+// const notify = (options) => {
+//   if (Vue.prototype.$isServer) return
 
-  const instance = new NotificationConstructor({})
-  id = `notification_${seed++}`
-  instance.id = id
-  instance.$mount()
-  document.body.appendChild(instance.vm.$el)
-}
+//   const instance = new NotificationConstructor({})
+//   id = `notification_${seed++}`
+//   instance.id = id
+//   instance.$mount()
+//   document.body.appendChild(instance.vm.$el)
+// }
